12a4a730795ff06b395ce6e9fc6637e3b968cee5
Adama Camara <48602776+acamara2016@users.noreply.github.com>
2022-05-02 02:39:34 -0300

    Update api.config.js

--- src/config/api.config.js
+++ src/config/api.config.js
@@ -1,5 +1,5 @@
 const config = {
-    api_key: "4caf30ffa3252eef2b9fa4dbc2f5554a"
+    api_key: ""
 }
 const api = {
     authentication:`https://api.themoviedb.org/3/authentication/token/new?api_key=${config.api_key}`,
